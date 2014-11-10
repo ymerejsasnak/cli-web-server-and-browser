7cc9fd554182aba7adea47dbdd4ba3e8373cf6bf
Jeremy A Kansas <ymerejsasnak@gmail.com>
2014-11-09 21:11:02 -0500

    working on getting browser to send POST request

--- browser.rb
+++ browser.rb
@@ -1,18 +1,27 @@
 require 'socket'
+require 'json'
 
 class Browser
-	attr_reader :host, :port, :method, :path, :request
+	attr_reader :host, :port, :method, :path, :post_request
 	  
   def initialize
 	  @host = "localhost"
     @port = 2000
     @method = ""
     @path = ""
+    @post_request = ""
   end
 
-  def send_request
+  def send_get_request
     socket = TCPSocket.open(host, port)
     request = "#{method} #{path} HTTP/1.0\r\nFrom: jk@mail.com\r\nUser-Agent: JKbrowser\r\nDate: #{Time.now}\r\n\r\n"
+    socket.print(request)
+    return socket.read.split("\r\n\r\n")
+  end
+
+  def send_post_request(post_request)
+    socket = TCPSocket.open(host, port)
+    request = "#{method} #{path} HTTP/1.0\r\nFrom: jk@mail.com\r\nUser-Agent: JKbrowser\r\nDate: #{Time.now}\r\n\r\n#{post_request}"
     socket.print(request)
     return socket.read.split("\r\n\r\n")
   end
@@ -32,11 +41,19 @@ class Browser
   def get_user_input
   	puts
   	puts "Type GET followed by the name of the resource to GET said resource."
-  	#puts  "Type POST .. .. .. . . .(to be done..)"
+  	puts "Type POST followed by your -name- and -email address- to POST that information."
   	puts
   	input = gets.chomp.split(" ")
   	@method = input[0]
-  	@path = input[1]
+  	if @method == "GET"
+  		@path = input[1]
+  		display_response(send_get_request)
+  	elsif @method == "POST"
+      name = input[1]
+      email = input[2]
+      post_request = {:user => {:name => "#{name}", :email => "#{email}"}}.to_json
+      display_response(send_post_request(post_request))
+  	end
   end
 end
 
@@ -44,4 +61,4 @@ end
 
 browser = Browser.new
 browser.get_user_input
-browser.display_response(browser.send_request)
+
